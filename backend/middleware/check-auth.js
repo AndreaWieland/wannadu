0fa2eaab099a3fc0d28fe358a71c0bf0c2d95a5a
Andrea Wieland <13319601+AndreaWieland@users.noreply.github.com>
2018-10-15 12:58:47 -0700

    taking out secret info

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    jwt.verify(token, "secret_this_should_be_longer");
+    jwt.verify(token, [secret token goes here, this code will not work until you add your own]);
     next();
   } catch (error) {
     res.status(401).json({ message: "Auth failed!" });
